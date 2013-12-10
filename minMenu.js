9ffc15ff8c5b540fb34ac16a07de7d9665410226
cgcgbcbc <cgcgbcbc@163.com>
2013-12-11 01:35:29 +0800

    add area param for delegate

--- minMenu.js
+++ minMenu.js
@@ -2,9 +2,11 @@
   $.fn.minMenu = function(options){
     var opts = $.extend( {}, $.fn.minMenu.defaults, options );
     var menu = $(this);
+    var param;
     menu.css('position','absolute');
     menu.hide();
     $(opts.area,document.body).on('click',function(){
+      param = $(this).attr('menu-param');
       var x = event.pageX;
       var y = event.pageY;
       event.stopPropagation();
@@ -17,13 +19,13 @@
         var menuId = $this.attr('menu-id');
         var menuData = $this.attr('menu-data');
         $this.on('click',{menuId:menuId,menuData:menuData},function(event){
-          opts.delegate(event.data.menuId,event.data.menuData);
+          opts.delegate(event.data.menuId,event.data.menuData,param);
         });
       });
     });
   };
   $.fn.minMenu.defaults = {
       area:'body',
-      delegate: function(menuId,menuData){}
+      delegate: function(menuId,menuData,param){}
   };
 })(jQuery)
