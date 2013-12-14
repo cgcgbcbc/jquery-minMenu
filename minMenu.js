d6348d59c24eded41657276428aee4b61f8a3e9c
Cheng Guang <cgcgbcbc@mulab.tsinghua.edu.cn>
2013-12-15 01:18:51 +0800

    Revert "Fix menu display when in the edge of the screen"
    This reverts commit cbe323f679bd499d13f6fc09a92d14ff534b05ff.

--- minMenu.js
+++ minMenu.js
@@ -1,54 +1,31 @@
-(function ($) {
-    $.fn.minMenu = function (options) {
-        var opts = $.extend({}, $.fn.minMenu.defaults, options);
-        var menu = $(this);
-        var param;
-        menu.css('position', 'absolute');
-        menu.hide();
-        $(opts.area, document.body).on('click', function (event) {
-            if ($(opts.area, event.target)) {
-                param = $(this).attr('menu-param');
-                var x = event.pageX;
-                var y = event.pageY;
-                var menuWidth = menu.prop('scrollWidth');
-                var menuHeight = menu.prop('scrollHeight');
-                var screenWidth = document.body.scrollWidth;
-                var screenHeight = document.body.scrollHeight;
-                if (x + menuWidth > screenWidth) x = screenWidth - menuWidth - 2;
-                if (y + menuHeight > screenHeight) y = screenHeight - menuHeight - 2;
-                event.stopPropagation();
-                menu.offset({
-                    left: x,
-                    top: y
-                });
-                menu.show();
-            }
+(function($){
+  $.fn.minMenu = function(options){
+    var opts = $.extend( {}, $.fn.minMenu.defaults, options );
+    var menu = $(this);
+    var param;
+    menu.css('position','absolute');
+    menu.hide();
+    $(opts.area,document.body).on('click',function(event){
+      param = $(this).attr('menu-param');
+      var x = event.pageX;
+      var y = event.pageY;
+      event.stopPropagation();
+      menu.offset({left:x,top:y});
+      menu.show();
+    });
+    return this.each(function(){
+      $('.menu-item',menu).each(function(){
+        var $this = $(this);
+        var menuId = $this.attr('menu-id');
+        var menuData = $this.attr('menu-data');
+        $this.on('click',{menuId:menuId,menuData:menuData},function(event){
+          opts.delegate(event.data.menuId,event.data.menuData,param);
         });
-        return this.each(function () {
-            $('.menu-item', menu).each(function () {
-                $this = $(this);
-                var menuId = $this.attr('menu-id');
-                var menuData = $this.attr('menu-data');
-                $this.on('click', {
-                    menuId: menuId,
-                    menuData: menuData
-                }, function (event) {
-                    opts.delegate(event.data.menuId, event.data.menuData, param);
-                });
-            });
-        });
-    };
-    $.fn.minMenu.defaults = {
-        area: 'body',
-        delegate: function (menuId, menuData, param) {}
-    };
-})(jQuery);
-
-$('.menu').minMenu({
-    area: '.area',
-    delegate: function (menuId, menuData, param) {
-        if (menuId == "1") alert(1);
-        else if (menuId == "2") alert(menuData);
-        else if (menuId == "3") alert(param);
-    }
-});
+      });
+    });
+  };
+  $.fn.minMenu.defaults = {
+      area:'body',
+      delegate: function(menuId,menuData,param){}
+  };
+})(jQuery)
